a9ba89825b13eb2ef0c24c40b0eda3d53065aa26
Bartlomiej Plotka <bwplotka@gmail.com>
2021-09-14 16:28:48 +0200

    Cleanup, trying different thing.
    Signed-off-by: Bartlomiej Plotka <bwplotka@gmail.com>

--- http_red_monitoring_kprobes.h
+++ http_red_monitoring_kprobes.h
@@ -25,30 +25,35 @@ BPF_HASH(active_sock_addr, u64, struct addr_info_t);
 // args is from /sys/kernel/debug/tracing/events/syscalls/sys_enter_accept4/format
 TRACEPOINT_PROBE(syscalls, sys_enter_accept4) {
     u64 id = bpf_get_current_pid_tgid();
-    u32 pid = id >> 32;
 
-//   struct addr_info_t addr_info;
-//   addr_info.addr = args->upeer_sockaddr;
-//   addr_info.addrlen = args->upeer_addrlen;
-//   active_sock_addr.update(&id, &addr_info);
+   struct addr_info_t addr_info;
+   addr_info.addr = args->upeer_sockaddr;
+   addr_info.addrlen = args->upeer_addrlen;
+   active_sock_addr.update(&id, &addr_info);
 
-    // bpf_trace_printk("accept %d\\n", bpf_get_current_cgroup_id());
     return 0;
 }
 
-
-// Read the sockaddr values and write to the output buffer.
 TRACEPOINT_PROBE(syscalls, sys_exit_accept4) {
+    u64 id = bpf_get_current_pid_tgid();
+
+    struct addr_info_t* addr_info = active_sock_addr.lookup(&id);
+    if (addr_info == NULL) {
+        active_sock_addr.delete(&id);
+        return 0;
+    }
+
     // The file descriptor is the value returned from the syscall.
     u64 fd = (u64)args->ret; // Somehow return argument is long type in my kernel.
-        if (fd < 0) {
-            return 0;
-        }
+    if (fd < 0) {
+        return 0;
+    }
 
-        bool t = true;
-        tracked_fds.update(&fd, &t);
+    bool t = true;
+    tracked_fds.update(&fd, &t);
+    bpf_trace_printk("accept %d\\n", fd);
 
-        requests_started_total.increment((u64) bpf_get_current_cgroup_id());
+    requests_started_total.increment((u64) bpf_get_current_cgroup_id());
 
 //  u64 id = bpf_get_current_pid_tgid();
 //  u32 pid = id >> 32;
@@ -62,13 +67,15 @@ TRACEPOINT_PROBE(syscalls, sys_enter_write) {
     if (tracked_fds.lookup(&fd) == NULL) {
         return 0;
     }
+    bpf_trace_printk("enter write %d\\n", fd);
     // bpf_trace_printk("write");
     requests_total.increment((u64) bpf_get_current_cgroup_id());
     return 0;
 }
 
 TRACEPOINT_PROBE(syscalls, sys_enter_close) {
-    //    tracked_fds.delete(&(args->fd));
+    u64 fd = args->fd;
+    tracked_fds.delete(&fd);
 
     //bpf_trace_printk("close");
     return 0;
